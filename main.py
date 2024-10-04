49a10f104ab0d8b0de79d097de7370f7dda84b04
cosmickitten <shadar171@gmail.com>
2024-10-04 14:27:26 +0300

    dev branch write to buffer

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
+import io
 import hashlib
 from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
+
+
+
 
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -16,29 +20,17 @@ if os.path.exists(dotenv_path):
     token = os.environ.get("token")
     ids = os.environ.get("ids").split(',')
     filename = os.environ.get("filename")
+    hashfilename = os.environ.get("hashfilename")
     group = os.environ.get("group")
 
+file_buffer= io.BytesIO()
+file_buffer.name=filename
 filename = dotenv_path = os.path.join(os.path.dirname(__file__), filename)
 
-
-def checkfile():
-    isExists = os.path.exists(filename) 
-    print(f"[+] Файл {filename} существует: {isExists}")
+def checkfile(file):
+    isExists = os.path.exists(file) 
+    print(f"[+] Файл {file} существует: {isExists}")
     return isExists
-
-def hashfile():
-    
-    sha256 = hashlib.sha256()
-    with open(filename, 'rb') as f:      
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha256.update(data)
-    sha256hex =  sha256.hexdigest()
-    print(f"[+] SHA256 = {sha256hex}")
-    return sha256hex
- 
 
 
 
@@ -67,10 +59,48 @@ def downloadFile(url):
     url = BASE_URL + url
     response = requests.get(url)
     print(f"[+] Найден файл  {url}")
-    with open(filename, mode="wb") as file:
-        file.write(response.content)
-    print(f"[+] Загружен файл {filename}")
+    file_buffer.write(response.content)
+    file_buffer.seek(0)
+    print(f"[+] Загружен файл {file_buffer.name}")
+    return file_buffer
 
+
+def getFileHash(file):
+    with open(hashlib.__file__, "rb") as filebuffer:
+        digest = hashlib.file_digest(filebuffer, "sha256")
+        filebuffer.seek(0)
+
+    sha256hex=digest.hexdigest()
+        
+    print(f"[+] SHA256 = {sha256hex}")
+    return sha256hex
+
+def writeHash(hash):
+    with open(hashfilename, mode="w") as file:
+        file.write(hash)
+ 
+    print(f"[+] Записан файл {hashfilename}")
+
+def readHashFromFile(file):
+    if checkfile(file):
+        with open(file, mode="r") as f:
+            hash = f.readline()
+        print(f"[+] SHA256 = {hash}")
+        return hash
+
+
+def sendFile(chatid,file):
+    print(f"[+] Отправляю файл {file}")
+    
+    data = {
+        'chat_id': (None, chatid),
+        'document':('shedule.pdf',io.BufferedReader(io.BytesIO(file.read())))
+        }
+    try:
+        response = requests.post(f'https://api.telegram.org/bot{token}/sendDocument', files=data)
+        print(f"[+] Файл {file} отправлен {chatid}")
+    except Exception as e:
+        print(e)
 
 def sendMessage(chatid,message):
     data = {
@@ -84,35 +114,29 @@ def sendMessage(chatid,message):
     except Exception as e:
         print(e)
 
-def sendFile(chatid):
-    print(f"[+] Отправляю файл {filename}")
-    
-    data = {
-        'chat_id': (None, chatid),
-        'document': open(filename, 'rb')
-        }
-    try:
-        response = requests.post(f'https://api.telegram.org/bot{token}/sendDocument', files=data)
-        print(f"[+] Файл {filename} отправлен {chatid}")
-    except Exception as e:
-        print(e)
-
-
-def sendFileToEverybody():
+def sendFileToEverybody(file):
     for chatid in ids:
         sendMessage(chatid,'Новое расписание!')
-        sendFile(chatid)
+        sendFile(chatid,file)
+
+
 
 
 if __name__ == "__main__":
-    if checkfile():
-        old=hashfile()
-        downloadFile(parsePage(getPage(url)))
-        new=hashfile()
-        if old != new:
+    if checkfile(hashfilename):
+        old_hash=readHashFromFile(hashfilename)
+        buffer=downloadFile(parsePage(getPage(url)))
+        new_hash=getFileHash(buffer)
+        if old_hash != new_hash:
             print("[+] Новое расписание")
             sendFileToEverybody()
+        else:
+            print(f"[+] old_hash = {old_hash} \n[+] new_hash = {new_hash}")
     else:
-        downloadFile(parsePage(getPage(url)))
-        sendFileToEverybody()
+        buffer=downloadFile(parsePage(getPage(url)))
+        hash=getFileHash(buffer)
+        writeHash(hash)
+        sendFileToEverybody(buffer)
+        
+        
     
